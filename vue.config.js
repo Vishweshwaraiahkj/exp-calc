7c7500a9c4bf7e3ef645487e6b3fee6907bbc10f
Vishweshwarayya Kj <k.vishu@outlook.com>
2022-07-01 14:50:41 +0530

    Exp-calc: added styles and icons to assets dir

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,30 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/',
+  assetsDir: 'src/assets',
+  lintOnSave: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        implementation: require('sass'),
+        sassOptions: {
+          indentedSyntax: false
+        },
+        additionalData: `
+            @import "src/assets/styles/_variables.scss";
+            @import "src/assets/styles/_global.scss";
+          `
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        fs: false,
+        path: false,
+        os: false
+      }
+    }
+  }
 })
